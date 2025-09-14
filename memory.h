99066c41e86686a596d627f6c0e035bb179233c1
m-ruff <156376977+m-ruff@users.noreply.github.com>
2025-09-14 19:08:59 -0400

    Added basic interface to memory

--- memory.h
+++ memory.h
@@ -1,6 +1,32 @@
 #ifndef MEMORY_H
 #define MEMORY_H
 
+#include <stdint.h>
+
+
+class MemoryModule {
+public:
+    // Constructor
+    MemoryModule(uint8_t cs, uint8_t wp, uint8_t clk,
+            uint8_t d0, uint8_t d1, uint8_t d2, uint8_t d3);
+
+    // Init the memory interface
+    void init();
+
+    // Read memory by address
+    int readByAddr(size_t addr, char * buffer);
+
+    // Write memory by address (Backdoor only)
+    void writeByAddr(const uint8_t * buf, size_t size);
+
+    // Read memory by DID
+    int readById(size_t identifier, char * buffer);
+
+    // Write memory by DID (Backdoor only)
+    void writeById(const uint8_t * buf, size_t identifier);
+
+}
+
 
 
 #endif
