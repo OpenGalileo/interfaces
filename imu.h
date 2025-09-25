df9884008ca554330dd7add2ccf9ee7c76ef0893
nowakra <146386799+nowakra@users.noreply.github.com>
2025-09-24 22:23:03 -0400

    Update imu.h

--- imu.h
+++ imu.h
@@ -5,64 +5,26 @@
 // IMU has accelerometer and gyroscope to read velocity, acceleration, and orientation
 
 class IMU {
-
+private: 
+   
 public:
+    void CalibrateIMU();
+    //call imu calibrate enable spin/any other setup the IMU needs
 
-    // Read velocity in m/s
-    void readVelocity() {
-        //  read data
-        //  convert raw accel data to m/s^2
-        //  calculate velocity
-        //  return velocity
-    }
+    void readQuaternion();
+    //  read IMU output  --> this comes in the form of a 4 element vector, this is orientation for math purposes 
 
-    // Read acceleration in m/s^2
-    void readAcceleration() {
-        //  read regs
-        //  combine MSB + LSB → signed int16 values
-        //  convert raw LSB → m/s^2 using formula:
-        //  accel_mps2 = raw_value * (range_in_g / 32768) * 9.81
-        //  return accel values for X, Y, Z
-    }
-
-    void readXaxis() {
-        //  read data regs
-        //  convert raw data to deg/s
-        //  calculate angle
-        //  return angle
-    }
-
-    void readYaxis() {
-        //  read gyro regs
-        //  convert raw data to deg/s
-        //  calculate angle
-        //  return angle
-    }
-
-    void readZaxis() {
-        //  read data regs
-        //  convert raw data to deg/s
-        //  calculate angle
-    }
-
-    // take full measurement of velocity, acceleration, and orientation
-    void takeMeasurement() {
-        //  trigger burst read
-        //  read accel and gyro data
-        //  convert raw data
-        //  update velocity and orientation state
-        //  store latest results internally
-    }
+    void readEuler();
+    //reads roll, pitch, and yaw
 
     // dont draw current
-    void sleepyTime() {
-
-    }
-
+    void sleep();
+    //dont pull imu to save power
+    
     // wake to be idle/normal
-    void WAKEUP() {
-
-    }
+    void wakeup();
+    //start pulling imu
+    
 };
 
 #endif
