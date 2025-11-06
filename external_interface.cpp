338de8f1b0537087841e2d81aa2e6d1d20d331cb
simonpi429 <simonpi@umich.edu>
2025-11-06 10:02:03 -0500

    Refactor I2C read/write methods and improve error handling

--- external_interface.cpp
+++ external_interface.cpp
@@ -1,4 +1,9 @@
-#include "external_interface.h"
+/*
+TODO: Decide if we want to only open and close the file once
+*/
+
+
+#include "external_interface.hpp"
 #include <iostream>
 #include <unistd.h>
 #include <fcntl.h>
@@ -9,28 +14,74 @@ StarTracker::StarTracker(){
 
 }
 
-StarTracker::~StarTracker(){
+//StarTracker::~StarTracker(){
 
+//}
+void StarTracker::set_mode(uint8_t mode){
+    uint8_t buf[1] = {mode};
+    bool ret = StarTracker::write_i2c(buf, 1, 0x00);
+    if(!ret){
+        std::cerr << "Failed mode write\n";
+    }
 }
 
 std::array<float,3>  StarTracker::get_imu_euler(){
+    uint8_t buf[6];
+    bool ret = StarTracker::read_i2c(buf, 6, 0x0C);
+    if(!ret){
+        std::cerr << "Failed euler read\n";
+        return {0.0f, 0.0f, 0.0f};
+    }
+    std::array<float,3> imu_euler = {0.0f, 0.0f, 0.0f};
+	int16_t temp = buf[0] | (int16_t)buf[1]<<8;
+    imu_euler[0] = (float)(temp/16.0f);
+	temp = buf[2] | (int16_t)buf[3]<<8;
+    imu_euler[1] = (float)(temp/16.0f);
+	temp = buf[4] | (int16_t)buf[5]<<8;
+    imu_euler[2] = (float)(temp/16.0f);
+    return imu_euler;
+}
+
+
+bool StarTracker::read_i2c(uint8_t *buf, size_t size, uint8_t addr){
     const char* device = "/dev/i2c-1";
-    int file = open(device, O_RDWR);
+    int file = open(device,O_RDWR);
     if (file < 0) {
         std::cerr << "Failed to open I2C device\n";
-        return nullptr;
+        return false;
     }
-    if (ioctl(file, I2C_SLAVE, 0x18) < 0) {
+    if (ioctl(file, I2C_SLAVE, addr) < 0) {
         std::cerr << "Failed to set I2C address\n";
         close(file);
-        return nullptr;
+        return false;
     }
-    std::array<float,3> imu_euler = {0.0f, 0.0f, 0.0f};;
-    uint8_t buf[6];
-    read(file,buf, 6);
+    ssize_t byteRead = read(file,buf, size);
+    if(byteRead < 0){
+        close(file);
+        return false;
+    }
     close(file);
-    imu_euler[0] = (float)((buf[0] + buf[1] <<8)/16);
-    imu_euler[1] = (float)((buf[2] + buf[3] <<8)/16);
-    imu_euler[2] = (float)((buf[4] + buf[5] <<8)/16);
-    return imu_euler;
+    return true;
+}
+
+bool StarTracker::write_i2c(uint8_t *buf, size_t size, uint8_t addr){
+    const char* device = "/dev/i2c-1";
+    int file = open(device, O_WRONLY);
+    if (file < 0) {
+        std::cerr << "Failed to open I2C device\n";
+        return false;
+    }
+    if (ioctl(file, I2C_SLAVE, addr) < 0) {
+        std::cerr << "Failed to set I2C address\n";
+        close(file);
+        return false;
+    }
+    ssize_t byteWrite = write(file,buf, size);
+    if(byteWrite < 0){
+        close(file);
+        return false;
+    }
+    close(file);
+    return true;
+
 }
