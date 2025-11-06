3540ac1924ea1433f1dac8f4473442561dca0091
simonpi429 <simonpi@umich.edu>
2025-11-06 10:01:33 -0500

    Add mode variable to StarTracker main loop

--- external_main_test.cpp
+++ external_main_test.cpp
@@ -7,7 +7,15 @@
 
 int main(){
     StarTracker star; 
+    uint8_t mode = IDLE_PI_OFF; 
     while(1){
+        star.set_mode(mode);
+        if(mode >=4){
+            mode = 0;
+        }
+        else{
+            mode++;
+        }
         std::array<float,3> imu_euler = star.get_imu_euler();
         std::cout<<imu_euler[0]<<" "<<imu_euler[1]<<" "<<imu_euler[2]<<std::endl; 
         sleep(3);
