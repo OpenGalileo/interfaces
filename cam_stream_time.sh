cdfe3b5ee15d60ad28958ce20df65fa539e7bbff
Matthew Szabo <mrszabo@umich.edu>
2025-11-23 19:21:56 -0500

    new cam stuff I hope

--- cam_stream_time.sh
+++ cam_stream_time.sh
@@ -1,26 +1,44 @@
 #!/bin/bash
-# Usage: ./cam_stream_time.sh EXPOSURE (in ms) GAIN (1-16) filename.png (all optional)
-EXPOSURE_MS=${1:-500} # Exposure in ms
-GAIN=${2:-4} #1-16
-OUTPUT_FILE=${3:-img.png}
 
-STREAM_PID=$(pgrep -f "rpicam-vid --nopreview --timeout 0")
-if [ -z "$STREAM_PID" ]; then
-    echo "Starting persistent stream..."
-    rpicam-vid --nopreview --timeout 0 --framerate 1 -o /dev/null &
-    STREAM_PID=$!
-    sleep 2
-else
-    echo "Persistent stream already running (PID $STREAM_PID)"
-fi
+# long_exposure_timelapse.sh
 
-TIMEOUT_MS=$((EXPOSURE_MS + 200))
-CMD="rpicam-still --shutter $((EXPOSURE_MS * 1000)) --gain $GAIN --timeout $TIMEOUT_MS -o $OUTPUT_FILE"
+# Bash script to take long exposures with IMX477 HQ camera on Pi Zero using rpicam-still
 
+# Usage: ./long_exposure_timelapse.sh <exposure_ms> <gain> <output_prefix> <num_shots>
+
+# Example: ./long_exposure_timelapse.sh 1000 4 img 5
+
+# -----------------------
+
+# Set defaults
+
+# -----------------------
+
+EXPOSURE_MS=${1:-1000}        # Exposure time in milliseconds
+GAIN=${2:-4}                  # Analog gain (1-16)
+OUT_PREFIX=${3:-img}          # Output file prefix
+NUM_SHOTS=${4:-1}             # Number of exposures
+
+# -----------------------
+
+# Loop over shots
+
+# -----------------------
+
+for ((i=1; i<=NUM_SHOTS; i++)); do
+OUT_FILE=$(printf "%s_%03d.png" "$OUT_PREFIX" "$i")
+TIMEOUT_MS=$((EXPOSURE_MS + 200))  # Slightly longer than shutter
+
+CMD="rpicam-still --shutter $((EXPOSURE_MS * 1000)) --gain $GAIN --timeout $TIMEOUT_MS --timelapse 0 -o $OUT_FILE -n"
+
+echo "Capturing shot $i/$NUM_SHOTS: $EXPOSURE_MS ms exposure to $OUT_FILE with gain $GAIN..."
 START_TIME=$(date +%s.%N)
+
+# Execute the capture command
 $CMD
+
 END_TIME=$(date +%s.%N)
 ELAPSED=$(echo "$END_TIME - $START_TIME" | bc)
-printf "Capture complete, total time: %.2f seconds\n" "$ELAPSED"
+printf "Shot %d complete! Total time: %.2f seconds\n\n" "$i" "$ELAPSED"
 
-pkill -P $STREAM_PID
+done
